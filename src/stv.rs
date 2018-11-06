8009a970610ff38467778ea351a138ab3f932b24
phayes <patrick.d.hayes@gmail.com>
2018-11-06 13:16:50 -0800

    Fixing STV - vote weights and ranking had some bugs

--- src/stv.rs
+++ src/stv.rs
@@ -1,23 +1,41 @@
 use std::hash::Hash;
 use std::collections::HashMap;
 
+use indexmap::IndexMap;
+
 #[derive(Debug)]
 struct WeightedVote<T: Eq + Clone + Hash + std::fmt::Debug> {
     weight: f64,
     remaining: Vec<T>,
 }
 
-pub struct STVTally<T: Eq + Clone + Hash + std::fmt::Debug> {
-    running_total: HashMap<T, Vec<WeightedVote<T>>>,
-    num_winners: u32,
+pub enum Quota {
+
+    // Droop Quota. If you are unsure what to use, use this one.
+    Droop,
+
+    // Hare quota.
+    Hare,
+
+    // Hagenbach-Bischoff quota.
+    Hagenbach
 }
 
-impl<T: Eq + Clone + Hash + std::fmt::Debug> STVTally<T>  {
+pub struct Tally<T: Eq + Clone + Hash + std::fmt::Debug> {
+    running_total: HashMap<T, Vec<WeightedVote<T>>>,
+    num_winners: u32,
+    quota: Quota,
+    candidates: HashMap<T, usize> // Map candiates to a unique integer identifiers.
+}
 
-    pub fn new(num_winners: u32) -> Self {
-        return STVTally {
+impl<T: Eq + Clone + Hash + std::fmt::Debug> Tally<T>  {
+
+    pub fn new(num_winners: u32, quota: Quota) -> Self {
+        return Tally {
             running_total: HashMap::new(),
             num_winners: num_winners,
+            quota: quota,
+            candidates: HashMap::new()
         };
     }
 
@@ -26,7 +44,8 @@ impl<T: Eq + Clone + Hash + std::fmt::Debug> STVTally<T>  {
             return;
         }
 
-        // TODO: Use pointers into a master-key Vec<T> (clone only once!).
+        // Ensure all selections are in the candidates list
+        self.mapped_candidates(&selection);
         
         let mut remaining = selection.clone();
         let choice = remaining.remove(0);
@@ -46,23 +65,34 @@ impl<T: Eq + Clone + Hash + std::fmt::Debug> STVTally<T>  {
         }
     }
 
-    pub fn result(&mut self) -> Vec<T> {
-        let threshold = self.droop_threshold();
+    pub fn result(&mut self) -> IndexMap<T, u32> {
+        let threshold = match self.quota {
+            Quota::Droop => self.droop_threshold(),
+            Quota::Hare => self.hare_threshold(),
+            Quota::Hagenbach => self.hagenbach_threshold(),
+        };
 
-        let mut results: Vec<T> = Vec::new();
+        let mut results: IndexMap<T, u32> = IndexMap::new();
 
+        let mut rank: u32 = 0;
         loop {
             // Check if any candidates are over the threshold
             let mut new_winners: Vec<T> = Vec::new();
             for (candidate, votes) in self.running_total.iter() {
-                if votes.len() >= threshold { // TODO count with weight
+                let mut votecount = 0.0;
+                for vote in votes.iter() {
+                    votecount += vote.weight;
+                }
+                if votecount as usize >= threshold {
                     new_winners.push(candidate.clone());
                 }
             }
 
             // If we have enough winners, end the tally and return results.
             if (results.len() + new_winners.len()) as u32 > self.num_winners {
-                results.append(&mut new_winners);
+                for winner in new_winners.drain(0..) {
+                    results.insert(winner, rank);
+                }
                 return results;
             }
             // If there's new winners, redistribute their excess vote.
@@ -70,7 +100,7 @@ impl<T: Eq + Clone + Hash + std::fmt::Debug> STVTally<T>  {
                 for winner in new_winners.drain(0..) {
                     let mut votes = self.running_total.remove(&winner).unwrap();
                     let overvote = votes.len() - threshold;
-                    let weight = overvote as f64 / threshold as f64;
+                    let weight = overvote as f64 / votes.len() as f64;
                     
                     // TODO: feature-gate num-rational instead of f64 for determinism
 
@@ -93,13 +123,16 @@ impl<T: Eq + Clone + Hash + std::fmt::Debug> STVTally<T>  {
                             }
                         }
                     }
-                    results.push(winner);
+                    results.insert(winner, rank);
                 }
 
                 // If we have enough results, return it.
                 if results.len() as u32 >= self.num_winners {
                     return results;
                 }
+
+                // We've added winners, so increase the rank and continue to the next round.
+                rank = rank + 1;
                 continue;
             }
             else {
@@ -107,7 +140,7 @@ impl<T: Eq + Clone + Hash + std::fmt::Debug> STVTally<T>  {
                 let mut new_loosers: Vec<T> = Vec::new();
                 {
                     let mut votecounts: HashMap<&T, f64> = HashMap::new();
-                    let mut least: f64 = 0.0;
+                    let mut least: f64 = std::f64::INFINITY;
                     for (candidate, votes) in self.running_total.iter() {
                         let mut votecount: f64 = 0.0;
 
@@ -125,9 +158,17 @@ impl<T: Eq + Clone + Hash + std::fmt::Debug> STVTally<T>  {
                             new_loosers.push(candidate.clone());
                         }
                     }
-                }
+                };
 
-                // TODO: What to do if, once we subtract the loosers, we don't have enough candidates left?
+                // If the number of loosers to be removed would result in an underelection, then the loosers become winners.
+                let needed_winners = self.num_winners as usize - results.len();
+                let available_winners = self.running_total.len() - new_loosers.len();
+                if available_winners < needed_winners {
+                    for winning_loosers in new_loosers.drain(0..) {
+                        results.insert(winning_loosers, rank);
+                    }
+                    return results;
+                }
 
                 // If there's new winners, redistribute their excess vote.
                 if new_loosers.len() > 0 {
@@ -157,6 +198,22 @@ impl<T: Eq + Clone + Hash + std::fmt::Debug> STVTally<T>  {
         }
     }
 
+    // Ensure that candidates are in our list of candidates, and return an internal numeric representation of the same
+    // TODO: Can we use a trait for this?
+    fn mapped_candidates(&mut self, selection: &Vec<T>) -> Vec<usize> {
+        let mut mapped = Vec::<usize>::new();
+        for selected in selection.iter() {
+            if self.candidates.contains_key(&selected) {
+                mapped.push(*self.candidates.get(&selected).unwrap());
+            }
+            else {
+                let len = self.candidates.len();
+                self.candidates.insert(selected.clone(), len);
+            }
+        }
+        return mapped;
+    }
+
     fn total_votes(&self) -> usize {
         let mut total: usize = 0;
 
@@ -169,6 +226,10 @@ impl<T: Eq + Clone + Hash + std::fmt::Debug> STVTally<T>  {
 
     fn droop_threshold(&self) -> usize {
         (self.total_votes() / (self.num_winners as usize+ 1)) + 1
+    }
+
+    fn hagenbach_threshold(&self) -> usize {
+        self.total_votes() / (self.num_winners as usize+ 1)
     }
 
     fn hare_threshold(&self) -> usize {
@@ -184,12 +245,35 @@ mod tests {
     #[test]
     fn stv_test() {
         // Election between Alice, Bob, and Cir
-        let mut tally = STVTally::new(2);
+        let mut tally = Tally::new(2, Quota::Droop);
         tally.add(vec!["Alice", "Bob", "Cir"]);
         tally.add(vec!["Alice", "Bob", "Cir"]);
         tally.add(vec!["Alice", "Bob", "Cir"]);
 
         let result = tally.result();
-        assert_eq!(result, vec!["Alice", "Bob"]);
+        assert_eq!(result, indexmap!{"Alice" => 0, "Bob" => 1});
+    }
+
+    #[test]
+    fn stv_wikipedia_test() {
+        // From https://en.wikipedia.org/wiki/Single_transferable_vote#Counting_the_votes
+        let mut tally = Tally::new(3, Quota::Droop);
+        for _ in 0..4 {
+            tally.add(vec!["Orange"]);
+        }
+        for _ in 0..2 {
+            tally.add(vec!["Pear", "Orange"]);
+        }
+        for _ in 0..8 {
+            tally.add(vec!["Chocolate", "Strawberry"]);
+        }
+        for _ in 0..4 {
+            tally.add(vec!["Chocolate", "Sweets"]);
+        }
+        tally.add(vec!["Strawberry"]);
+        tally.add(vec!["Sweets"]);
+
+        let result = tally.result();
+        assert_eq!(result, indexmap!{"Chocolate" => 0, "Orange" => 1, "Strawberry" => 2});
     }
 }
